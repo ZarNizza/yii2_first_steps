afda4043f63eac62d87a15acd90be1ecdbbf413f
ZarNizza <gormaster@mail.ru>
2022-08-18 22:41:40 +0400

    alytics - initial commit

--- views/site/index.php
+++ views/site/index.php
@@ -1,51 +1,100 @@
 <?php
 /* @var yii\web\View $this */
-$this->title = 'My Yii Application';
+$this->title = 'My 1st Yii Application';
 ?>
-<div class="site-index">
+<div class = 'site-index'>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<div class = 'jumbotron'>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+<h2>Сервис проверки доступности url-ов</h2>
+<h3>Тестовое задание: php-разработчик</h3>
+<p>Yii2 фреймворк</p>
+</div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+<div class = 'body-content'>
 
-    <div class="body-content">
+<div class = 'row'>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+<div class = 'col-lg-4'>
+<h3>Необходимо реализовать:</h3>
+<p>
+<ul>
+<li>
+<b>Пользовательскую часть</b> ( localhost ) - форма создания ссылки на проверку со следующим набором полей:
+<ul>
+<li>url для проверки</li>
+<li>частота проверки ( каждые 1, 5, 10 минут )</li>
+<li>количество повторов в случае ошибки ( от 0 до 10 )</li>
+</ul>
+</li>
+<li><b>Административную часть</b> ( localhost/admin/ ) в интерфейсе которой можно:
+<ul>
+<li>просматривать список добавленных url-ов:<br/>
+- дата создания<br/>
+- url<br/>
+- частота проверки<br/>
+- количество повторов<br/>
+</li>
+<li>просматривать список проверок:<br/>
+- дата проверки<br/>
+- url<br/>
+- http-код<br/>
+- номер попытки<br/>
+</li>
+</ul>
+</li>
+<li><b>Backend</b>, который будет с заданной частотой проверять url’ы. Задержка между повторами в случае ошибки - 1 минута.
+</li>
+</ul>
+</p>
+</div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+<div class = 'col-lg-4'>
+<h3>Требования
+к&nbsp;
+реализации:</h3>
+<p>
+<ul>
+<li>Приложение на php-фреймворке ( крайне желательно <b>yii2</b> )</li>
+<li>Миграция структуры данных средствами фреймворка</li>
+<li>Приложение разрабатывать в <b>git</b> репозитории - начальный коммит в ветке master с форком ( или клоном репозитория ) фреймворка, реализация задания в отдельной ветке</li>
+</ul>
+</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+</div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+<div class = 'col-lg-4'>
+<h3>Опционально:</h3>
+<p>
+<ul>
+<li>Возможность проверять <b>100+</b> url'ов в минуту, с соблюдением заданной частоты для каждого url’a (допускается погрешность до 10 секунд)</li>
+<li>Реализация с <b>rabbitmq</b> (или другие очереди на выбор)</li>
+<li><b>Dockerfile</b> + docker-compose</li>
+</ul>
+</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+</div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+</div>
+<hr/>
+<div class = 'row'>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
+<div class = 'col-lg-4'>
+    <p><a class = 'btn btn-default' href = 'http://www.yiiframework.com/doc/'>Yii Documentation &raquo;
+</a></p>
+</div>
 
-    </div>
+<div class = 'col-lg-4'>
+    <p><a class = 'btn btn-default' href = 'http://www.yiiframework.com/forum/'>Yii Forum &raquo;
+</a></p>
+</div>
+
+<div class = 'col-lg-4'>
+    <p><a class = 'btn btn-default' href = 'http://www.yiiframework.com/extensions/'>Yii Extensions &raquo;
+</a></p>
+</div>
+
+</div>
+
+</div>
 </div>
